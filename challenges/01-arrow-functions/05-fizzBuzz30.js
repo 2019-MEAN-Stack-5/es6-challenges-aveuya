f7d6c92f8963f460f2110812eabda7a3d41ec6ce
averyjb26@gmail.com <averyjb26@gmail.com>
2019-09-16 21:46:12 -0700

    completed 05-fizzBuzz30

--- challenges/01-arrow-functions/05-fizzBuzz30.js
+++ challenges/01-arrow-functions/05-fizzBuzz30.js
@@ -1,11 +1,27 @@
-//assign fizzBuzz30 an arrow function that does not take any inputs.  fizzBuzz30 should output an array of all the numbers from
-//1 through 30 replacing numbers divisible by 3 with 'fizz', numbers divisible by 5 with 'buzz' and numbers divisible by 3 and 5 with
-//'fizzbuzz'
+// //assign fizzBuzz30 an arrow function that does not take any inputs.  
+// fizzBuzz30 should output an array of all the numbers from
+// //1 through 30 replacing numbers divisible by 3 with 'fizz', numbers 
+// divisible by 5 with 'buzz' and numbers divisible by 3 and 5 with
+// //'fizzbuzz'
 
-let fizzBuzz30;
 
+let fizzBuzz30 = () => {
+    var fizzBuzz = [];
+    for (let num = 1; num < 30; num++) {
+        if (num % 15 === 0) {
+            fizzBuzz.push('fizzbuzz');
+        } else if (num % 5 === 0) {
+            fizzBuzz.push('buzz');
+        } else if (num % 3 === 0) {
+            fizzBuzz.push('fizz');
+        } else {
+            fizzBuzz.push(num);
+        }
+    }
+    return fizzBuzz;
+}
 
-
+console.log(fizzBuzz30());
 
 
 
