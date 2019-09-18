c53cbb39e21f81d1c0113c854913bb7eba8e885d
averyjb26@gmail.com <averyjb26@gmail.com>
2019-09-17 20:35:01 -0700

    completed 01-string

--- challenges/02-template-literals/01-string.js
+++ challenges/02-template-literals/01-string.js
@@ -1,10 +1,10 @@
 // Convert the below string to make use of teplate literals
 
-function string() {
-    return 'Marry had a little lamb';
-}
+// function string() {
+//     return 'Marry had a little lamb';
+// }
 
-
+let string = () => `Mary had a little lamb`;
 
 
 
