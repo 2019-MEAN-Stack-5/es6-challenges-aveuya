e7eeda32fd56f9b29b7877ee9e80a869d8397516
averyjb26@gmail.com <averyjb26@gmail.com>
2019-09-18 18:13:32 -0700

    completed 04-stringWithArray

--- challenges/02-template-literals/04-stringWithArray.js
+++ challenges/02-template-literals/04-stringWithArray.js
@@ -1,11 +1,16 @@
-//Complete the below function to output a string using template literals. The string should read: The second item in this array is _____.
-//Make use of interpolation to include the 2nd item from the array input in place of ____. Ex.  The second item in the array is orange.
+// //Complete the below function to output a string using template literals. 
+// The string should read: The second item in this array is _____.
+// //Make use of interpolation to include the 2nd item from the array 
+// input in place of ____. Ex.  The second item in the array is orange.
+
+// const stringWithArray = (array) => {
+    
+// };
 
 const stringWithArray = (array) => {
-    
+    let item = array[1];
+    return `The second item in this array is ${item}.`
 };
-
-
 
 
 
